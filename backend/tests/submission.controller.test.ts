0046e6a2e4728d469444096676444bb997aa0abe
shiv <sk.sk.shivukumar@gmail.com>
2026-02-15 10:30:00 +0530

    chore: setup test mocks

--- backend/tests/submission.controller.test.ts
+++ backend/tests/submission.controller.test.ts
@@ -5,6 +5,23 @@ import * as workerService from '../src/services/workerService';
 
 jest.mock('../src/services/submissionService');
 
+import { pool } from '../src/db/client';
+
 describe('SubmissionController', () => {
+
+  beforeAll(async () => {
+    await pool.query('SELECT 1');
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  jest.spyOn(workerService, 'processSubmission')
+      .mockResolvedValue(undefined);
 
 });
