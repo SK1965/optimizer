23ce905e45c34656890d2ae6db6fb38d6bf56e91
shiv <sk.sk.shivukumar@gmail.com>
2026-02-22 22:35:09 +0530

    feat: add cors to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response,} from 'express';
+import cors from 'cors';
 import router from './routes/submission.routes';
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/', router);
 app.get('/', async (req: Request, res: Response) => {
